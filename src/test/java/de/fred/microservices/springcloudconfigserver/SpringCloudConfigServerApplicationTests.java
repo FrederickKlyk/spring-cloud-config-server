e725692209c0ef15254b805212d717dc92e39357
Frederick Klyk <klyk@adesso-mobile.de>
2024-02-06 16:32:39 +0100

    configuration port and name

---
+++ src/test/java/de/fred/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fred.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
